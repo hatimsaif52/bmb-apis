3977b5d6171c9bf8b36f02227645432ec026cc82
Moiz Zulfiqar <37923177+hatimsaif52@users.noreply.github.com>
2025-10-27 15:12:43 +0500

    CORS issue fix

--- api/reorder.js
+++ api/reorder.js
@@ -1,6 +1,7 @@
 export default async function handler(req, res) {
     // Handle CORS
   res.setHeader('Access-Control-Allow-Origin', 'https://brandmybeverage.com/');
+  res.setHeader('Access-Control-Allow-Origin', 'https://brandmybeverage.com');
   res.setHeader('Access-Control-Allow-Methods', 'POST, OPTIONS');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
 
